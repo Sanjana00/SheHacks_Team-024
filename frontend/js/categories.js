e672e8e79f53cfe2368bb7e634c6d3d5ece32645
Chibi-girl <lucysanzenin@gmail.com>
2021-03-13 23:39:16 +0530

    added link to jobs page in categories

--- frontend/js/categories.js
+++ frontend/js/categories.js
@@ -1,18 +1,23 @@
 
-const button = document.getElementById('post-btn');
-
-button.addEventListener('click', async _ => {
-var location=val1, type_of_work=val2, field=val3; 
+function getjobs()
+{
+var location=val1, type_of_work=val2, field=val3,obj; 
  axios(`http://localhost:3000/`, {			//and sends a post request with inputted parameters
 				 "method": "POST",
 				 "headers": {"content-type": "application/json"},
   				"data": JSON.stringify({location,type_of_work,field})
   				})
       	.then((res) => {
-      	var obj=res.data;
+      	obj=res.data;
 		 console.log(obj[0]);		
+		 localStorage.setItem("jobs", JSON.stringify(obj));
 		})
 		.catch(error => console.error(error));
-		})
+
+		setTimeout(function(){ window.location.href = "jobs.html";}, 1000);
+
+}
+		
+
 
 
