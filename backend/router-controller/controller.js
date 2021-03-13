c7f2ef76976e86b44505a429af724e7f375a5ee4
Chibi-girl <lucysanzenin@gmail.com>
2021-03-13 14:18:33 +0530

    Added function for fetching filtered joblist

--- backend/router-controller/controller.js
+++ backend/router-controller/controller.js
@@ -3,7 +3,7 @@ const Job = require('../models/jobs_model.js')
 const {check,validation}= require('express-validator');
 
 exports.getJobs = (req, res) => {		
-Job.find({}, (err, jobs) => {
+Job.find({duration: req.body.duration , location:req.body.location, type_of_work:req.body.type_of_work, level_of_experience: req.body.level_of_experience, hours_per_week: req.body.hours_per_week }, (err, jobs) => {
         if (err) {
             res.send(err);
         }
